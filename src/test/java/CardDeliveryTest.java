d3ce758ff6e3a428f5500253b319b3bbf85052a0
ksenya31 <kseniakalinnikova268@gmail.com>
2022-10-09 15:40:46 +0300

    change test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -22,18 +22,18 @@ public class CardDeliveryTest {
         $(".button").shouldHave(Condition.text("Запланировать")).click();
         $("[data-test-id=success-notification]").shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на " + scheduledDate),
-                        Duration.ofSeconds(14));
+                        Duration.ofSeconds(15));
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        String rescheduledDate = DataGenerator.generateDate(14);
+        String rescheduledDate = DataGenerator.generateDate(15);
         $("[data-test-id=date] input").setValue(rescheduledDate);
         $(".button").shouldHave(Condition.text("Запланировать")).click();
         $("[data-test-id=replan-notification]").shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Необходимо подтверждение" +
                                 " У вас уже запланирована встреча на другую дату. Перепланировать?"),
-                        Duration.ofSeconds(14));
+                        Duration.ofSeconds(15));
         $("[data-test-id=replan-notification] .button").shouldHave(Condition.text("Перепланировать")).click();
         $("[data-test-id=success-notification]").shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на " + rescheduledDate),
-                        Duration.ofSeconds(14));
+                        Duration.ofSeconds(15));
     }
 }
