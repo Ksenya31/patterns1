868466533362ab57848d2b1c1ab659d69e0720c5
ksenya31 <kseniakalinnikova268@gmail.com>
2022-10-09 09:00:10 +0300

    initial commit

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Condition;
+import netology.ru.DataGenerator;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+    @Test
+    public void shouldSuccessfulFormSubmission() {
+        open("http://localhost:9999");
+        DataGenerator.UserInfo userData = DataGenerator.Registration.generateUser("Ru");
+        $("[data-test-id=city] input").setValue(userData.getCity());
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String scheduledDate = DataGenerator.generateDate(3);
+        $("[data-test-id=date] input").setValue(scheduledDate);
+        $("[data-test-id=name] input").setValue(userData.getName());
+        $("[data-test-id=phone] input").setValue(userData.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(".button").shouldHave(Condition.text("Запланировать")).click();
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на " + scheduledDate),
+                        Duration.ofSeconds(14));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String rescheduledDate = DataGenerator.generateDate(14);
+        $("[data-test-id=date] input").setValue(rescheduledDate);
+        $(".button").shouldHave(Condition.text("Запланировать")).click();
+        $("[data-test-id=replan-notification]").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Необходимо подтверждение" +
+                                " У вас уже запланирована встреча на другую дату. Перепланировать?"),
+                        Duration.ofSeconds(14));
+        $("[data-test-id=replan-notification] .button").shouldHave(Condition.text("Перепланировать")).click();
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на " + rescheduledDate),
+                        Duration.ofSeconds(14));
+    }
+}
---
+++ src/test/java/netology/ru/DataGenerator.java
@@ -0,0 +1,60 @@
+package netology.ru;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+import lombok.Value;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift)                          //Текущая дата плюс shift дней
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+    }
+
+    public static String generateCity() {
+        final Random random = new Random();
+        final String [] citiesAdministrativeCenters = new String [] {"Новотроицк", "Горно-Алтайск", "Уфа", "Улан-Удэ",
+                "Махачкала", "Орск", "Москва", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь",
+                "Йошкар-Ола", "Саранск", "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный",
+                "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь",
+                "Владивосток", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород",
+                "Брянск", "Владимир", "Волгоград", "Вологда", "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга",
+                "Кемерово", "Киров", "Кострома", "Курган", "Курск", "Гатчина", "Липецк", "Магадан", "Красногорск",
+                "Мурманск", "Нижний Новгород", "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза",
+                "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург", "Смоленск",
+                "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск", "Ярославль", "Москва",
+                "Санкт-Петербург", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"};
+        return citiesAdministrativeCenters[random.nextInt(85)];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
